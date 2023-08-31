f77936bcc719903f82dd508466add4153897fc56
Ton <ton@jarrbo.nl>
2023-08-31 11:13:00 +0200

    Add UserAdmin with search_fields for custom model To prevent errors in migrate

--- admin.py
+++ admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')  # whatever
+    search_fields = ('first_name', 'last_name')
+
+admin.site.register(User, UserAdmin)
